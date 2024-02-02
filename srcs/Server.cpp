c25e032c6e86f9f2b64d41cfc428010c12099057
alvgomezv <alkitegv@gmail.com>
2024-02-02 14:20:58 +0100

    First draft of a simple server created

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,7 +1,78 @@
 #include "../include/Server.hpp"
+#include <iostream>
+#include <sys/socket.h>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 100
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    int server_fd;
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		std::cerr << "Socket creation error" << std::endl;
+		return 1;
+	}
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+	address.sin_port = htons(8080);
+
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		std::cerr << "Bind failed" << std::endl;
+		return 1;
+	}
+
+	if (listen(server_fd, 3) < 0)
+	{
+		std::cerr << "Listen failed" << std::endl;
+		return 1;
+	}
+	
+	std::cout << "Waiting for a connection on port 8080" << std::endl;
+	
+	while (1)
+	{
+		int new_socket;
+
+		if ((new_socket = accept(server_fd, (struct sockaddr *)NULL, NULL)) < 0)
+		{
+			std::cerr << "Accept failed" << std::endl;
+			return 1;
+		}
+
+		std::string httpRequest;
+
+		while (true) 
+		{
+			char buffer[BUFFER_SIZE];
+			ssize_t bytesRead = recv(new_socket, buffer, BUFFER_SIZE - 1, 0);
+			if (bytesRead == -1)
+			{
+				std::cerr << "Error reading from socket" << std::endl;
+				close(new_socket);
+				break;
+			}
+
+			buffer[bytesRead] = '\0';
+			httpRequest += buffer;
+
+			if (bytesRead < BUFFER_SIZE - 1) {
+				std::cout << "Received complete HTTP Request:\n" << httpRequest << std::endl;
+
+				const char* response = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, World!";
+				send(new_socket, response, strlen(response), 0);
+
+				break;
+			}
+
+			close(new_socket);
+		}
+	}
     return 0;
 }

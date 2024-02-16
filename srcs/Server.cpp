729911b2032b284117ec908d25917606c0688d9c
alvgomezv <alkitegv@gmail.com>
2024-02-16 16:34:06 +0100

    Fixed the merge a bit, create new branch
    New branch to work on implementing the request into the Connection
    Manager

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -51,17 +51,9 @@ int Server::getSocket() const
 {
 	return _server_socket;
 } 
-#include "../include/Request.hpp"
-
-#include <iostream>
-#include <fstream>
-#include <string>
 
 
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <iomanip>
+// PARTE DE FERNANDO
 
 // std::map<std::string, std::string> decodeUrlEncodedFormData(const std::string& encodedFormData)
 // {
@@ -136,46 +128,3 @@ int Server::getSocket() const
 // 	 return 0;
 // }
 
-
-int main(int argc, char** argv)
-{
-	std::cout << GREEN "Usage: ./server [get|post|delete]" << std::endl << "Default: post" RESET << std::endl;
-	const char* filename = "http_raw/post.txt";
-	if (argc == 2)
-	{
-		if (strcmp(argv[1], "get") == 0)
-			filename = "http_raw/get.txt";
-		else if (strcmp(argv[1], "post") == 0)
-			filename = "http_raw/post.txt";
-		else if (strcmp(argv[1], "delete") == 0)
-			filename = "http_raw/delete.txt";
-		else
-			return (std::cerr << RED "Invalid argument" RESET << std::endl, 1);
-	}
-
-	FILE* archivo = fopen(filename, "rb");
-	if (archivo == NULL)
-	{
-		std::cerr << RED "Error: Can't open the file" RESET<< std::endl;
-		return 1;
-	}
-
-	RequestHandler requestHandler;
-	const int buffer_size = 8; // Tamaño del buffer en bytes
-	char buffer[buffer_size];
-	std::string buffer_str;
-	std::string content;
-	while (fgets(buffer, buffer_size, archivo) != NULL)
-	{
-		buffer_str = buffer;
-		content+= buffer_str;
-		if (requestHandler.parseRequest(buffer_str))
-			break;
-	}
-	fclose(archivo);
-
-	std::cout << std::endl << YELLOW "HTTP request raw content:" RESET << std::endl << content << std::endl << std::endl;
-
-	std::cout << YELLOW "HTTP request parsed:" RESET << std::endl << requestHandler << std::endl;
-	return 0;
-}

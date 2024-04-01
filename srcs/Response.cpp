c41f7841a897b225bb090472acde2303eb827d9f
fgomez-d <fgomez-d@student.42malaga.com>
2024-04-01 18:49:41 +0200

    Fix trailing slashes and root directory with alias

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -156,7 +156,9 @@ void	Response::buildHttpResponse()
 	// Check if there is a location that matches the requested URL
 	_location = matchLocation();
 	if (!_location)
+	{
 		return (setErrorResponse(404));
+	}
 	// Handle redirects
 	if (_location->getRedirCode() > 0)
 		return (setRedirection());
@@ -223,7 +225,15 @@ Location	*Response::matchLocation()
 		}
 	}
 	if (max_loc_size)
+	{
+		if (max_loc_size == 1)
+			return (matched_loc);
+		if (req_path.size() > max_loc_size && req_path[max_loc_size] != '/')
+		{
+			return (NULL);
+		}
 		return (matched_loc);
+	}
 	return (NULL);
 }
 
@@ -433,8 +443,13 @@ std::string	Response::buildFilesystemPath(std::string request_path)
 
 	if (!alias.empty())
 	{
-		filesystem_path = request_path.substr(_location->getPath().size(), std::string::npos);
-		filesystem_path = root + alias + filesystem_path;
+		if (_location->getPath() != "/")
+		{
+			filesystem_path = request_path.substr(_location->getPath().size(), std::string::npos);
+			filesystem_path = root + alias + filesystem_path;
+		}
+		else
+			filesystem_path = root + alias + request_path;
 		return (filesystem_path);
 	}
 	filesystem_path = root + request_path;
@@ -445,7 +460,11 @@ std::string	Response::buildFilesystemPath(std::string request_path)
 void	Response::handleGetDirectory(std::string filesys_dir_path)
 {
 	// Check if the location has an index defined
-	std::string filesys_index_path = filesys_dir_path + "/" + _location->getIndex();
+	std::string filesys_index_path;
+	if (filesys_dir_path == "/")
+		filesys_index_path = filesys_dir_path + _location->getIndex();
+	else
+		filesys_index_path = filesys_dir_path + "/" + _location->getIndex();
 	struct stat fileStat;
 	if (access(filesys_index_path.c_str(), F_OK) == 0
 		&& stat(filesys_index_path.c_str(), &fileStat) == 0 
